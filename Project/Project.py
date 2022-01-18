379730c543be5e170dbfd0407700409f169b198e
Pedro Carvalho <pmdc@ua.pt>
2022-01-18 17:09:49 +0000

    SIFT

--- Project/Project.py
+++ Project/Project.py
@@ -9,6 +9,7 @@
 
 import argparse
 import cv2
+from matplotlib import pyplot as plt
 import numpy as np
 from numpy.lib.function_base import copy
 import os
@@ -67,7 +68,7 @@ def main():
     image = cv2.imread("./Imagens/Puzzle_X_ordenado_v2.jpeg", cv2.IMREAD_COLOR)
     
     window_name = "Peças separadas"
-    image = cv2.resize(image, (600, 600))   # Temos demasiada resolução da câmara
+    image = cv2.resize(image, (800, 800))   # Temos demasiada resolução da câmara
     image_aux = image.copy()
     # image_aux = cv2.resize(image_aux, (800, 800))
     masker=maskmaker(image)
@@ -143,8 +144,9 @@ def main():
         cv2.imwrite(os.path.join(pathBW , "BW"+str(i)+".jpg"), cropped_bw)
         cv2.imwrite(os.path.join(pathColored , "Colored"+str(i)+".jpg"), cropped_color)
     
-    image_piece=cv2.imread("./Colored/Colored0.jpg")
-    image_piece_gray= cv2.cvtColor(image_piece,cv2.COLOR_BGR2GRAY)
+    # image_piece=cv2.imread("./Colored/Colored0.jpg")
+    # image_piece_gray= cv2.cvtColor(image_piece,cv2.COLOR_BGR2GRAY)
+
     # --- Corner Harris --- #
     # dst=cv2.cornerHarris(image_piece_gray,4,5,0.13)
     # dst = cv2.dilate(dst,None)
@@ -163,16 +165,85 @@ def main():
     # print(position_of_all_corners)
 
     # --- SIFT --- #
-    imagebase=cv2.imread("./Imagens/Puzzle_X_completo_v2.jpeg",cv2.IMREAD_GRAYSCALE)
+    # imagebase=cv2.imread("./Imagens/Puzzle_X_completo_v2.jpeg",cv2.IMREAD_GRAYSCALE)
 
-    sift = cv2.SIFT_create()
-    kp = sift.detect(image_piece_gray,None)
-    img=cv2.drawKeypoints(image_piece_gray,kp,image)
+    # sift = cv2.SIFT_create()
+    # kp = sift.detect(image_piece_gray,None)
+    # img=cv2.drawKeypoints(image_piece_gray,kp,image)
 
 
-    keypoints_1, descriptors_1 = sift.detectAndCompute(imagebase,None)
-    print(keypoints_1,descriptors_1)
-    cv2.imshow("SIFT",img)
+    # keypoints_1, descriptors_1 = sift.detectAndCompute(imagebase,None)
+    # print(keypoints_1,descriptors_1)
+    # cv2.imshow("SIFT",img)
+
+    # read images
+    img5 = cv2.imread('./Imagens/Puzzle_X_completo_v2.jpeg')  
+    img6 = cv2.imread('./Colored/Colored9.jpg') 
+
+    img1 = cv2.cvtColor(img5, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img6, cv2.COLOR_BGR2GRAY)
+
+    #sift
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+    #     good.append(m)
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+
+    MIN_MATCH_COUNT = 10
+
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        d,h= img1.shape[::-1]
+        w=1
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+
+        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+    else:
+        print ("Not enough matches are found" )
+        matchesMask = None
+
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                    singlePointColor = None,
+                    matchesMask = matchesMask, # draw only inliers
+                    flags = None)
+
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+
+
+    # #feature matching
+    # bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+    # matches = bf.match(descriptors_1,descriptors_2)
+    # matches = sorted(matches, key = lambda x:x.distance)
+
+    # img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:20], img2, flags=2)
+    # print(len(matches))
+    plt.imshow(img3), plt.show()
+
     cv2.waitKey(-1)
 
     cv2.destroyAllWindows
